e2db76b5e20c1e49c7cac6992ab8db675916a287
Daniel Frey <daniel._.frey@hotmail.com>
2022-04-20 16:17:46 -0300

    add angular material e flex design

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolvrine' },
+  { id: 2, name: 'Ciclop' },
+  { id: 3, name: 'Spyck' },
+  { id: 4, name: 'Lincinegra' },
+  { id: 5, name: 'Noturno' },
+  { id: 6, name: 'X-Men' },
+  { id: 7, name: 'Din Gray' },
+  { id: 8, name: 'Xavier' },
+  { id: 9, name: 'Tempestade' },
+  { id: 10, name: 'Vampira' },
+];
