c22314cda5d7b318eb4f8668c5c99d6a13ffac10
Daniel Frey <daniel._.frey@hotmail.com>
2022-04-25 15:17:21 -0300

    Reorganizado arquivos p/ serem mais independentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    DashboardComponent,
-
-  ],
+  declarations: [AppComponent],
   imports: [
+    //@angular
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
 
+    // feature
+    DashboardModule,
+    HeroesModule,
+
+    //app
     CoreModule,
     AppRoutingModule,
   ],
