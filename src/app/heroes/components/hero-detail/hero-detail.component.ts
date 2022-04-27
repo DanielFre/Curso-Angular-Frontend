13a2e999c37bc20d670d5ab1cda620d840e403c9
Daniel Frey <daniel._.frey@hotmail.com>
2022-04-27 10:48:09 -0300

    add metodo put, ajustado get e metodo trim()

--- src/app/heroes/components/hero-detail/hero-detail.component.ts
+++ src/app/heroes/components/hero-detail/hero-detail.component.ts
@@ -9,22 +9,36 @@ import { HeroService } from '../../../core/services/hero.service';
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.scss'],
 })
-export class HeroDetailComponent implements OnInit{
+export class HeroDetailComponent implements OnInit {
   hero!: Hero;
   constructor(
     private heroService: HeroService,
     private location: Location,
     private route: ActivatedRoute
   ) {}
+
   ngOnInit(): void {
-      this.getHero();
-  }
-  getHero(): void{
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id).subscribe(hero=> this.hero=hero);
+    this.getHero();
   }
 
-  goBack(): void{
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getOne(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
     this.location.back();
+  }
+
+  //não permite espaços em branco no inicio
+    //se vier vazio = ''
+    // !! nega  o vazio 1x = ! => true
+    // !! nega  o vazio 2x = !! => false
+  isFormValid(): boolean {
+    return !!this.hero.name.trim();
+  }
+
+  save(): void {
+    this.heroService.update(this.hero).subscribe(() => this.goBack());
   }
 }
